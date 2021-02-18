5129f25f8eda7ad921071d1390a377259a63af68
jofeisy <70162280+jofeisy@users.noreply.github.com>
2021-02-17 23:00:47 -0400

    Header created from home

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
       <div className="container mt-5">
         <h1>Tareas</h1>
         <hr></hr>
+        <div className="row">
+          <div className="col-8">
+            <h4 className="text-center">Lista de Tareas</h4>
+          </div>
+          <div className="col-4">
+
+          </div>
+        </div>
       </div>
     </div>
   )

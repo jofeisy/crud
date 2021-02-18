9eada32cd466ced5a6963f8ac2edd30222a01e3e
jofeisy <70162280+jofeisy@users.noreply.github.com>
2021-02-18 14:43:12 -0400

    Add task from work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { isEmpty } from 'lodash'
+import shortid from 'shortid'
 
 
 function App() {
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])  
+
+  const addTask = (e) => {
+    e.preventDefault()
+
+    if(isEmpty(task)){
+      console.log("Task empty")
+      return
+    }
+
+    const newTask = {
+      id: shortid.generate(),
+      task
+    }
+
+    setTasks([ ...tasks, newTask ])
+    setTask("")
+  }
+
   return (
     <div className="App">
       <div className="container mt-5">
@@ -10,9 +32,29 @@ function App() {
         <div className="row">
           <div className="col-8">
             <h4 className="text-center">Lista de Tareas</h4>
+            <ul className="list-group">
+              {
+                <li className="list-group-item">
+                  <span className="lead">Nombre de la tarea</span>
+                  <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+                  <button className="btn btn-warning btn-sm float-right">Editar</button>
+                </li>
+              }       
+            </ul>
           </div>
           <div className="col-4">
+          <h4 className="text-center">Formulario</h4>
 
+          <form onSubmit={addTask}>
+            <input 
+              type="text" 
+              className="form-control mb-2" 
+              placeholder="Ingrese la tarea..."
+              onChange={(text) => setTask(text.target.value)}
+              value={task}>              
+            </input>
+            <button type="submit" className="btn btn-dark btn-block">Agregar</button>
+          </form>
           </div>
         </div>
       </div>
